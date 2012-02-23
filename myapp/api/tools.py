b995714de7b423f79daca447f8a59d951e5f7d71
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-02-23 20:00:44 -0300

    //virst version of the tastytools implementation

---
+++ myapp/api/tools.py
@@ -0,0 +1,32 @@
+# myapp/api/tools.py
+from tastytools.api import Api
+from tastytools.test.resources import ResourceTestData
+from myapp.api.resources import EntryResource, UserResource
+
+
+class EntryTestData(ResourceTestData):
+    resource = "entry"
+
+    def get_data(self, data):
+        data.set('user', resource='user')
+        data.set('pub_date', '2010-12-24T06:23:48')
+        data.set('title', 'Lorem ipsum')
+        data.set('slug', 'lorem')
+        data.set('body', 'Lorem ipsum ad his scripta blandit partiendo...')
+        return data
+
+class UserTestData(ResourceTestData):
+    resource = "user"
+
+    def get_data(self, data):
+        data.set('username', 'foo')
+        data.set('email', 'bar@foo.com')
+        return data
+
+v1_api = Api(api_name='v1')
+v1_api.register(EntryResource())
+v1_api.register(UserResource())
+
+# register our test data
+v1_api.register_testdata(EntryTestData)
+v1_api.register_testdata(UserTestData)
--- urls.py
+++ urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from tastypie.api import Api
-from myapp.api.resources import EntryResource, UserResource
-
-v1_api = Api(api_name='v1')
-v1_api.register(UserResource())
-v1_api.register(EntryResource())
+from myapp.api.tools import v1_api
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^api/', include(v1_api.urls)),
+    (r'^tastytools/', include('tastytools.urls'), {'api_name': v1_api.api_name}),
 )
